6f63485c94aa222d7c52541f216074a792230351
Kevin Knights <kevinknights29@hotmail.com>
2024-04-21 16:10:08 -0500

    feat: added solution

---
+++ stack/155_min_stack/solution.py
@@ -0,0 +1,35 @@
+class MinStack:
+    def __init__(self):
+        self._stack = []
+        self._min_stack = []
+
+    def push(self, val: int) -> None:
+        self._stack.append(val)
+
+        if len(self._stack) == 1:
+            self._min_stack.append(val)
+        else:
+            self._min_stack.append(min(val, self._min_stack[-1]))
+
+    def pop(self) -> None:
+        self._stack.pop()
+        self._min_stack.pop()
+
+    def top(self) -> int:
+        return self._stack[-1]
+
+    def getMin(self) -> int:
+        return self._min_stack[-1]
+
+
+if __name__ == "__main__":
+    minStack = MinStack()
+    minStack.push(-2)
+    minStack.push(0)
+    minStack.push(-3)
+    assert minStack.getMin() == -3
+    minStack.pop()
+    assert minStack.top() == 0
+    assert minStack.getMin() == -2
+
+    print("All passed!")
